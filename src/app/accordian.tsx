81a9197fd26932c50fff2f9aa79caf274fb5bb6e
Vagish Tiwari <67112240+ringmasterrr@users.noreply.github.com>
2024-04-03 18:52:41 +0530

    Add files via upload

--- src/app/accordian.tsx
+++ src/app/accordian.tsx
@@ -1,5 +1,9 @@
 "use client"
 import React, { useState } from "react";
+interface AccordionProps {
+  title: string; // Specify the type explicitly as string
+  answer: string; // Assuming answer is also a string, change it accordingly if it's a different type
+}
 
 const Accordion = ({ title, answer }) => {
   const [accordionOpen, setAccordionOpen] = useState(false);
